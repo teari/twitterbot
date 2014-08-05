e0ef8df77b376c77ef9c722148832747a8c596fe
thayakawa <thayakawa@debian>
2014-08-05 16:35:24 +0900

    少し変更

--- twtto.py
+++ twtto.py
@@ -2,6 +2,9 @@
 import twitter
 import sys
 
+argvs = sys.argv
+argc = len(argvs)
+
 
 consumer_key             = ''
 consumer_secret          = ''
@@ -10,7 +13,13 @@ access_token_secret      = ''
 
 api = twitter.Api(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
-print "What are you doing?:"
-postmsg = sys.stdin.readline()
-
+if (argc == 1):
+    print "What are you doing?: "
+    postmsg = sys.stdin.readline()
+    quit()
+elif (argc != 2):
+    f = open(argvs[1])
+    data = "test"
+    postmsg = data
+    f.close
 status = api.PostUpdates(postmsg.decode("utf-8"))
