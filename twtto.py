d351100f4ab2574389f7837ae019f30756033185
thayakawa <thayakawa@debian>
2014-08-06 10:07:04 +0900

    keyをt_key.pyに格納

--- twtto.py
+++ twtto.py
@@ -1,22 +1,22 @@
 #coding:utf-8
 import twitter
+import t_key
 import sys
 
 argvs = sys.argv
 argc = len(argvs)
 
 
-consumer_key             = ''
-consumer_secret          = ''
-access_token_key         = ''
-access_token_secret      = ''
+consumer_key             = t_key.dict['consumer_key']
+consumer_secret          = t_key.dict['consumer_secret']
+access_token_key         = t_key.dict['access_token_key']
+access_token_secret      = t_key.dict['access_token_secret']
 
 api = twitter.Api(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
 if (argc == 1):
     print "What are you doing?: "
     postmsg = sys.stdin.readline()
-    quit()
 elif (argc != 2):
     f = open(argvs[1])
     data = "test"
